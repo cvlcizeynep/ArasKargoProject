785a7884a812637ede81e10a948eb22b95855fa9
Zeynep Cuvalci <cvlcizeynep@outlook.com>
2023-12-29 14:27:53 +0300

    00

---
+++ src/test/java/stepDefinitions/US07_StepDef.java
@@ -0,0 +1,91 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class US07_StepDef {
+
+
+
+    @Given("Kullanici urle gider")
+    public void kullanici_urle_gider() {
+
+    }
+    @When("Acilan sayfada alertler varsa kapatir")
+    public void acilan_sayfada_alertler_varsa_kapatir() {
+
+    }
+    @When("Sayfada sol asagi kisinda bulunan en yakin subeler butonuna tiklar")
+    public void sayfada_sol_asagi_kisinda_bulunan_en_yakin_subeler_butonuna_tiklar() {
+
+    }
+    @Then("En yakin subeler butonunun tiklanabilir oldugunu dogrular")
+    public void en_yakin_subeler_butonunun_tiklanabilir_oldugunu_dogrular() {
+
+    }
+
+
+    @When("Arama islemi seciniz okuna tiklar")
+    public void arama_islemi_seciniz_okuna_tiklar() {
+
+    }
+    @When("Acilan dropdowndan subelerimizi secer")
+    public void acilan_dropdowndan_subelerimizi_secer() {
+
+    }
+    @When("Konumla buldan arama yapar")
+    public void konumla_buldan_arama_yapar() {
+
+    }
+    @Then("Kulanici konumuna en yakin subelerin listelendigini dogrular")
+    public void kulanici_konumuna_en_yakin_subelerin_listelendigini_dogrular() {
+
+    }
+
+
+
+    @When("Il ilce seceneklerinden secim yaparak arama yapar")
+    public void ıl_ilce_seceneklerinden_secim_yaparak_arama_yapar() {
+
+    }
+    @Then("Secilen il ilceye gore listelenme yapildigini dogrular")
+    public void secilen_il_ilceye_gore_listelenme_yapildigini_dogrular() {
+    }
+
+    @When("Acilan dropdowndan Aras burasi secenegini tiklar")
+    public void acilan_dropdowndan_aras_burasi_secenegini_tiklar() {
+
+    }
+    @When("Esnaf modunu secili hale getirir")
+    public void esnaf_modunu_secili_hale_getirir() {
+
+    }
+    @When("Konumla aramayi secerek arama yapar")
+    public void konumla_aramayi_secerek_arama_yapar() {
+
+    }
+    @Then("Kullanici kendine en yakin Aras Burasi noktalarinin listelendigini dogrular")
+    public void kullanici_kendine_en_yakin_aras_burasi_noktalarinin_listelendigini_dogrular() {
+
+    }
+
+    @Then("Kullanici sectigi il ilceye gore Aras Burasi noktalarinin listelendigini dogrular")
+    public void kullanici_sectigi_il_ilceye_gore_aras_burasi_noktalarinin_listelendigini_dogrular() {}
+
+
+        @When("Dolap modunu secili hale getirir")
+        public void dolap_modunu_secili_hale_getirir () {
+
+        }
+
+
+
+
+
+
+
+
+
+
+    }
